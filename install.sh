aaee924b59d921d68c16ad29831ac4feb61a77f4
Mathias Johansson <mathias.johansson@duva.se>
2020-11-06 10:19:55 +0100

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Symlink config-files into home-directory
-ln -s .repos/pasoconfig/tmux.conf ~/.tmux.conf
-ln -s .repos/pasoconfig/vim ~/.vim
+ln -s ~/.repos/pasoconfig/tmux.conf ~/.tmux.conf
+ln -s ~/.repos/pasoconfig/vim ~/.vim
 
